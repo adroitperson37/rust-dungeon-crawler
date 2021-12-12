d3d59449d13aa1b8d06002449df83d609e3697cd
Rakesh Viparla <rakeshviparla@Rakeshs-MacBook-Pro.local>
2021-12-12 03:12:37 -0700

    added code so that systems use messages to intent using want_move system

--- src/systems/mod.rs
+++ src/systems/mod.rs
@@ -5,6 +5,7 @@ mod entity_render;
 mod collisons;
 mod random_move;
 mod end_turn;
+mod movement;
 
 
 //pub fn build_scheduler() -> Schedule {
@@ -29,6 +30,8 @@ pub fn build_input_scheduler()-> Schedule{
 
 pub fn build_player_scheduler()-> Schedule{
     Schedule::builder()
+        .add_system(movement::movement_system())
+        .flush()
         .add_system(collisons::collisions_system())
         .flush()
         .add_system(map_render::map_render_system())
@@ -40,7 +43,7 @@ pub fn build_monster_scheduler()-> Schedule{
     Schedule::builder()
         .add_system(random_move::random_move_system())
         .flush()
-        .add_system(collisons::collisions_system())
+        .add_system(movement::movement_system())
         .flush()
         .add_system(map_render::map_render_system())
         .add_system(entity_render::entity_render_system())
--- src/systems/player_input.rs
+++ src/systems/player_input.rs
@@ -1,9 +1,9 @@
 use crate::prelude::*;
 
 #[system]
-#[write_component(Point)]
+#[read_component(Point)]
 #[read_component(Player)]
-pub fn player_input(ecs:&mut SubWorld, #[resource]map: &Map,#[resource]key: &Option<VirtualKeyCode>,#[resource]camera: &mut Camera,#[resource]turn_state: &mut TurnState) {
+pub fn player_input(ecs:&mut SubWorld, commands: &mut CommandBuffer, #[resource]key: &Option<VirtualKeyCode>,#[resource]turn_state: &mut TurnState) {
     if let Some(key) = key {
         let delta = match key {
             VirtualKeyCode::Left => Point::new(-1, 0),
@@ -13,18 +13,27 @@ pub fn player_input(ecs:&mut SubWorld, #[resource]map: &Map,#[resource]key: &Opt
             _ => Point::new(0, 0),
             
         };
-        if delta.x!=0 || delta.y!=0 {
-            let mut players = <&mut Point>::query()
-                .filter(component::<Player>());
-            players.iter_mut(ecs).for_each(|pos| {
-                let destination = *pos+delta;
-                if map.can_enter_tile(destination){
-                    *pos = destination;
-                    camera.on_player_move(destination);
-                    *turn_state = TurnState::PlayerTurn;
-                }
+        let mut players = <(Entity,&Point)>::query()
+            .filter(component::<Player>());
+        players.iter(ecs).for_each(|(entity,pos)|{
+            let destination = *pos+delta;
+            commands.push(((),WantsToMove{entity: *entity,destination}));
         });
-    }
+        *turn_state = TurnState::PlayerTurn;
+//        if delta.x!=0 || delta.y!=0 {
+//            let mut players = <&mut Point>::query()
+//                .filter(component::<Player>());
+//            players.iter_mut(ecs).for_each(|pos| {
+//                let destination = *pos+delta;
+//                commands.push(((),WantsToMove{entity:*entity.destination}))
+////                if map.can_enter_tile(destination){
+////                    *pos = destination;
+////                    camera.on_player_move(destination);
+////                    *turn_state = TurnState::PlayerTurn;
+////                }
+//        });
+//                    *turn_state = TurnState::PlayerTurn;
+//    }
   }       
 }
 
